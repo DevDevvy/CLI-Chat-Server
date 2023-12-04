b5a2d510c8d7e25f13a8cd031ff4c1111c138676
DevDevvy <randallthomasmusic@gmail.com>
2023-12-03 19:17:22 -0800

    init

--- src/websocket/chat_room.go
+++ src/websocket/chat_room.go
@@ -2,6 +2,8 @@
 package websocket
 
 import (
+	"log"
+	"strings"
 	"sync"
 
 	"github.com/gorilla/websocket"
@@ -9,14 +11,15 @@ import (
 
 // ChatRoom represents a chat room with connected clients
 type ChatRoom struct {
-	clients map[*websocket.Conn]bool
-	mutex   sync.Mutex
+	clients   map[*websocket.Conn]bool
+	usernames map[*websocket.Conn]string
+	mutex     sync.Mutex
 }
 
-// NewChatRoom creates a new ChatRoom instance
 func NewChatRoom() *ChatRoom {
 	return &ChatRoom{
-		clients: make(map[*websocket.Conn]bool),
+		clients:   make(map[*websocket.Conn]bool),
+		usernames: make(map[*websocket.Conn]string),
 	}
 }
 
@@ -25,6 +28,15 @@ func (cr *ChatRoom) AddClient(client *websocket.Conn) {
 	cr.mutex.Lock()
 	defer cr.mutex.Unlock()
 	cr.clients[client] = true
+
+	// Retrieve the username associated with the client
+	username, exists := cr.usernames[client]
+	if !exists {
+		log.Printf("Error: No username found for client %v", client)
+	}
+
+	log.Printf("Client %v (%s) added. Total clients: %d", client, username, len(cr.clients))
+
 }
 
 // RemoveClient removes a client from the chat room
@@ -34,17 +46,82 @@ func (cr *ChatRoom) RemoveClient(client *websocket.Conn) {
 	delete(cr.clients, client)
 }
 
-// Broadcast sends a message to all clients in the chat room, excluding the sender
+// SetUsername sets the temporary username for a connection
+func (cr *ChatRoom) SetUsername(conn *websocket.Conn, username string) {
+	cr.mutex.Lock()
+	defer cr.mutex.Unlock()
+	cr.usernames[conn] = username
+}
+
+// GetUserame gets the temporary username for a connection
+func (cr *ChatRoom) GetUsername(conn *websocket.Conn) string {
+	cr.mutex.Lock()
+	defer cr.mutex.Unlock()
+	return cr.usernames[conn]
+}
+
+// SendUserList sends the list of connected users to a specific user
+func (cr *ChatRoom) SendUserList(conn *websocket.Conn) {
+	cr.mutex.Lock()
+	defer cr.mutex.Unlock()
+
+	var userList []string
+	for _, username := range cr.usernames {
+		userList = append(userList, username)
+	}
+
+	message := "Connected Users: " + strings.Join(userList, ", ")
+	err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+	if err != nil {
+		log.Printf("Error sending user list to client: %v", err)
+	}
+}
+
+// BroadcastUserList sends the updated list of connected users to all clients
+func (cr *ChatRoom) BroadcastUserList() {
+	cr.mutex.Lock()
+	defer cr.mutex.Unlock()
+
+	var userList []string
+	for _, username := range cr.usernames {
+		userList = append(userList, username)
+	}
+
+	message := "Connected Users: " + strings.Join(userList, ", ")
+	for client := range cr.clients {
+		err := client.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			// Handle error (e.g., client disconnected)
+		}
+	}
+}
+
+// GetConnectedUserList returns a formatted list of connected users
+func (cr *ChatRoom) GetConnectedUserList() string {
+	cr.mutex.Lock()
+	defer cr.mutex.Unlock()
+
+	var userList []string
+	for _, username := range cr.usernames {
+		userList = append(userList, username)
+	}
+
+	return strings.Join(userList, ", ")
+}
+
+// Broadcast sends a message to all connected clients except the sender
 func (cr *ChatRoom) Broadcast(sender *websocket.Conn, message []byte) {
+	log.Println("Broadcast")
 	cr.mutex.Lock()
 	defer cr.mutex.Unlock()
 
 	for client := range cr.clients {
-		if client != sender {
-			err := client.WriteMessage(websocket.TextMessage, message)
-			if err != nil {
-				// Handle error (e.g., client disconnected)
-			}
+		log.Printf("client: %v", client)
+		// if client != sender {
+		err := client.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Printf("Error broadcasting message: %v", err)
+			// }
 		}
 	}
 }
--- src/websocket/manager.go
+++ src/websocket/manager.go
@@ -2,8 +2,11 @@
 package websocket
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
@@ -24,23 +27,98 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	log.Println("Client Connected")
-	chatRoom.AddClient(conn)
+	// Authenticate the user
+	err = authenticateUser(conn)
+	if err != nil {
+		log.Printf("Authentication error: %v", err)
+		return
+	}
+
+	// Prompt for a temporary username in a separate goroutine
+	go promptForUsername(conn)
+
+	// Announce the new user connection to all clients
+	username := chatRoom.GetUsername(conn)
+	announcement := fmt.Sprintf("%s has joined. Connected Users: %s", username, chatRoom.GetConnectedUserList())
+	chatRoom.Broadcast(conn, []byte(announcement))
+
+	// Send the initial list of connected users to the newly connected user
+	chatRoom.SendUserList(conn)
+
+	// Handle user input in a separate goroutine
+	go handleUserInput(conn, username)
+
+	// Broadcast the updated list of connected users when a user disconnects
 	defer func() {
 		log.Println("Client Disconnected")
 		chatRoom.RemoveClient(conn)
+		chatRoom.BroadcastUserList()
 	}()
 
+	// Keep the main goroutine alive until the connection is closed
+	select {}
+}
+
+// handleUserInput reads messages from the user and broadcasts them to others
+func handleUserInput(conn *websocket.Conn, username string) {
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Printf("Error reading message %v from client: %v", messageType, err)
+			log.Printf("Error reading %v message from client: %v", messageType, err)
 			return
 		}
 
 		log.Printf("Received message: %s", p)
 
-		// Broadcast the message to all clients in the chat room, excluding the sender
-		chatRoom.Broadcast(conn, p)
+		// Broadcast the message with the username prefix to all clients
+		chatRoom.Broadcast(conn, []byte(username+": "+string(p)))
 	}
+}
+
+// authenticateUser checks the user's password
+func authenticateUser(conn *websocket.Conn) error {
+	const correctPassword = "password"
+
+	err := conn.WriteMessage(websocket.TextMessage, []byte("Enter the password: "))
+	if err != nil {
+		return err
+	}
+
+	_, passwordBytes, err := conn.ReadMessage()
+	if err != nil {
+		return err
+	}
+
+	// Trim whitespaces from the received password
+	receivedPassword := strings.TrimSpace(string(passwordBytes))
+
+	// Check if the received message is not empty
+	if len(receivedPassword) == 0 {
+		return errors.New("password not provided")
+	}
+
+	fmt.Printf("Received password: %s\n", receivedPassword) // Print the received password
+
+	if receivedPassword != correctPassword {
+		return errors.New("incorrect password")
+	}
+
+	return nil
+}
+
+// promptForUsername prompts the user to enter a temporary username
+func promptForUsername(conn *websocket.Conn) {
+	err := conn.WriteMessage(websocket.TextMessage, []byte("Enter a temporary username: "))
+	if err != nil {
+		log.Printf("Error writing message to client: %v", err)
+		return
+	}
+
+	_, username, err := conn.ReadMessage()
+	if err != nil {
+		log.Printf("Error reading message from client: %v", err)
+		return
+	}
+
+	chatRoom.SetUsername(conn, string(username))
 }
